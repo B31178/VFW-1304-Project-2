d34fedde02c0c51b19f37dcc1dac1b2c6c0340b6
B31178 <travisp@fullsail.edu>
2013-04-18 17:32:58 -0400

    Begin Week 2.

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*
+Travis Pochintesta
+VFW 1304
+Project 2
+main.js
+*/
+
+/*
+var firstName = document.getElementById("firstName").value;
+var lastName = document.getElementById("lastName").value;
+var phoneNumber = document.getElementById("phoneNumber").value;
+var streetAddress = document.getElementById("streetAddress").value;
+var emailAddress = document.getElementById("emailAddress").value;
+
+document.addEventListener("event", function);
+
+document.setAttribute("attribute name", "attribute value");
+
+document.removeAttribute("attribute name", attribute value");
+
+.innerHtml;
+
+.createElement("string");
+
+.appendChild("string");
+
+*/
